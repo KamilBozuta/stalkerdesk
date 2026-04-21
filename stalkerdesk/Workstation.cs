aef780b1a3758b92dd2a2c09e0897c647d2e2844
ajajajaj231 <hub.zar180@gmail.com>
2026-04-21 12:47:50 +0200

    Update Workstation.cs

--- stalkerdesk/Workstation.cs
+++ stalkerdesk/Workstation.cs
@@ -1,8 +1,37 @@
-﻿namespace stalkerdesk
+using System.ComponentModel;
+
+namespace stalkerdesk
 {
-    public class Workstation
+    public class Workstation : INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public string IP { get; set; }
+        private string name;
+        private string ip;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public string IP
+        {
+            get => ip;
+            set
+            {
+                ip = value;
+                OnPropertyChanged(nameof(IP));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string prop)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
