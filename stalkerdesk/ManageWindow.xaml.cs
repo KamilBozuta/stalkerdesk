692b1ddbcd3edd76c15828d2f70257cadd07ba21
ajajajaj231 <hub.zar180@gmail.com>
2026-04-22 11:48:09 +0200

    Update ManageWindow.xaml.cs

--- stalkerdesk/ManageWindow.xaml.cs
+++ stalkerdesk/ManageWindow.xaml.cs
@@ -8,15 +8,19 @@ namespace stalkerdesk
     {
         private Workstation _pc;
         private Func<string, string, Task> _sendCommand;
+        private Func<string, Task> _showScreen;
 
-        public ManageWindow(Workstation pc, Func<string, string, Task> sendCommand)
+        public ManageWindow(
+            Workstation pc,
+            Func<string, string, Task> sendCommand,
+            Func<string, Task> showScreen)
         {
             InitializeComponent();
-
             _pc = pc;
             _sendCommand = sendCommand;
+            _showScreen = showScreen;
 
-            PcName.Text = $"{pc.Name} ({pc.IP})";
+            PcName.Text = pc.Name + " (" + pc.IP + ")";
         }
 
         private async void Shutdown_Click(object sender, RoutedEventArgs e)
@@ -32,6 +36,24 @@ namespace stalkerdesk
         private async void Lock_Click(object sender, RoutedEventArgs e)
         {
             await _sendCommand(_pc.IP, "lock");
+        }
+
+        private async void Timer_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(TimerBox.Text, out int minutes))
+            {
+                await _sendCommand(_pc.IP, "timer " + minutes);
+                MessageBox.Show("Ustawiono timer: " + minutes + " min");
+            }
+            else
+            {
+                MessageBox.Show("Podaj poprawną liczbę");
+            }
+        }
+
+        private async void Screen_Click(object sender, RoutedEventArgs e)
+        {
+            await _showScreen(_pc.IP);
         }
     }
 }
